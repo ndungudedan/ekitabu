a179a80a21f7bd618144e51cd50da081bc913e79
ndungudedan <dnkibere@gmail.com>
2021-08-21 18:36:23 +0300

    initializd for prohject

---
+++ app/src/main/java/com/dedan/ekitabu/ui/index/HomeActivity.kt
@@ -0,0 +1,21 @@
+package com.dedan.ekitabu.ui.index
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
+
+@Composable
+fun HomeScreen(){
+
+}
